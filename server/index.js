a4ce4f392c090c18fa7009288414893bf06cc6f7
Andrew Feng <bxandrew@gmail.com>
2023-02-18 22:05:54 -0500

    implementing a public folder to server static files for stress test from loader.io

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path')
 const cors = require('cors');
 const products = require('./routes/products.js');
 
@@ -8,6 +9,7 @@ const products = require('./routes/products.js');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/products', products);
 
